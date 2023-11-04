1d31d46c102c0a897824617e4e782e89df48f8ed
Stas <stas@iMac-Stas-3.local>
2023-11-04 14:45:41 +0100

    first Next app commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import AcmeLogo from '@/app/ui/acme-logo';
+import { useState } from "react";
 
 import Link from 'next/link';
+import { RadioGroup, RadioOption } from './ui/radio';
 export default function Page() {
+
+  //hool for radio group
+  const [selected, setSelected] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -9,6 +17,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -22,6 +31,21 @@ export default function Page() {
           >
             <span>Log in</span>
           </Link>
+        
+     
+    <div className="App">
+      <h2>How did you hear about Little Lemon?</h2>
+      <RadioGroup onChange={setSelected} selected={selected}>
+        <RadioOption value="social_media" checked={false} onChange={setSelected}>Social Media</RadioOption>
+        <RadioOption value="friends" checked={false} onChange={setSelected}>Friends</RadioOption>
+        <RadioOption value="advertising" checked={false} onChange={setSelected}>Advertising</RadioOption>
+        <RadioOption value="other" checked={false} onChange={setSelected}>Other</RadioOption>
+      </RadioGroup>
+      <button disabled={!selected}>Submit</button>
+    </div>
+
+
+
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
